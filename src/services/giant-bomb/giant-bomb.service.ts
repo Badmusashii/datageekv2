376e2092fd1895e6cc57fff14e55f864bb0c672c
Badmusashii <badmusashii@gmail.com>
2023-10-18 16:17:20 +0200

    ok

--- src/services/giant-bomb/giant-bomb.service.ts
+++ src/services/giant-bomb/giant-bomb.service.ts
@@ -44,30 +44,66 @@ export class GiantBombService {
       );
     }
   }
+  // async getAdditionalGameImages(guid: string) {
+  //   const imagesURL = `${this.baseURL}/images/${guid}/?api_key=${this.apiKey}&format=json&field_list=medium_url,small_url,original_url`;
+
+  //   try {
+  //     const response = await axios.get(imagesURL);
+
+  //     const smallImages = response.data.results.map((item) => item.small_url);
+  //     // Filtrer les images contenant "capture%20screenshot"
+  //     const filteredImages = smallImages.filter((url) =>
+  //       ['scree', 'scal'].some((str) => url.includes(str)),
+  //     );
+
+  //     // Mélanger le tableau
+  //     filteredImages.sort(() => Math.random() - 0.5);
+
+  //     if (filteredImages.length >= 6) {
+  //       const randomSixImages = filteredImages.slice(0, 6);
+  //       return randomSixImages;
+  //       // return smallImages;
+  //     } else {
+  //       // Sinon, on prend les 6 premières images au hasard
+  //       const randomSixImages = smallImages.slice(0, 6);
+  //       return randomSixImages;
+  //       // return smallImages;
+  //     }
+  //   } catch (error) {
+  //     throw new Error(`Failed to fetch additional images: ${error.message}`);
+  //   }
+  // }
   async getAdditionalGameImages(guid: string) {
-    const imagesURL = `${this.baseURL}/images/${guid}/?api_key=${this.apiKey}&format=json&field_list=medium_url,small_url,original_url`;
+    const imagesURL = `${this.baseURL}/images/${guid}/?api_key=${this.apiKey}&format=json&field_list=medium_url,small_url,original_url,image_tags`;
 
     try {
       const response = await axios.get(imagesURL);
 
-      const smallImages = response.data.results.map((item) => item.small_url);
-      // Filtrer les images contenant "capture%20screenshot"
-      const filteredImages = smallImages.filter((url) =>
-        ['scree', 'scal'].some((str) => url.includes(str)),
+      const allImages = response.data.results;
+
+      // Filtrer pour obtenir uniquement les screenshots
+      const screenshots = allImages.filter(
+        (image: { image_tags: string | string[] }) =>
+          image.image_tags && image.image_tags.includes('Screenshots'),
       );
 
-      // Mélanger le tableau
-      filteredImages.sort(() => Math.random() - 0.5);
+      // Si on a au moins 6 screenshots, on les utilise
+      if (screenshots.length >= 6) {
+        const randomSixScreenshots = screenshots
+          .slice(0, 6)
+          .map((item: { small_url: any }) => item.small_url);
+        return randomSixScreenshots;
+      }
 
-      if (filteredImages.length >= 6) {
-        const randomSixImages = filteredImages.slice(0, 6);
+      // Si on n'a pas suffisamment de screenshots, on utilise d'autres images
+      if (allImages.length >= 6) {
+        const randomSixImages = allImages
+          .slice(0, 6)
+          .map((item) => item.small_url);
         return randomSixImages;
-        // return smallImages;
       } else {
-        // Sinon, on prend les 6 premières images au hasard
-        const randomSixImages = smallImages.slice(0, 6);
-        return randomSixImages;
-        // return smallImages;
+        // Sinon, retourner toutes les images disponibles
+        return allImages.map((item: { small_url: any }) => item.small_url);
       }
     } catch (error) {
       throw new Error(`Failed to fetch additional images: ${error.message}`);
